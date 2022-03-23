c97a38b970333d00d8e15f9e995526e7de0b8d96
@budy6991 <alvmenfer@gmail.com>
2022-03-23 08:12:06 +0200

    Function for the grid created

--- script.js
+++ script.js
@@ -6,20 +6,34 @@ const cell = document.createElement('div')
 
 
 
-function grid (cell)  {
+
+// function grid (cell)  {
     
-    for (i= 0 ; i<255; i++){
-        cell = document.createElement('div')
-        cell.classList.add('cells')
-        container.appendChild(cell)
-        cell.addEventListener('mouseover', (e) => {
-            e.target.classList.add('hover')
-        })
+//     for (i= 0 ; i<255; i++){
+//         cell = document.createElement('div')
+//         cell.classList.add('cells')
+//         container.appendChild(cell)
+//         cell.addEventListener('mouseover', (e) => {
+//             e.target.classList.add('hover')
+//         })
+//     }
+    
+// }
+
+// grid (cell)
+
+
+function createGrid (columns, rows){
+
+    let grid = columns * rows;
+
+    for (let i = 0; i< grid; i++) {
+        let colRow = document.createElement ('div')
+        colRow.classList.add('colRow')
+        container.appendChild(colRow)
+        
     }
-    
+
 }
 
-grid (cell)
-
-
-
+createGrid(16,16);
